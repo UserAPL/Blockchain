d4c6678609409a033da1c3b673babea8c282097d
User/APL <arnapal.user@gmail.com>
2025-05-02 02:31:33 +0530

    {commit_message}

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import BrowseRouter from "./BrowseRouter";
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faInstagram,
+  faFacebookF,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faInstagram, faFacebookF, faLinkedinIn);
+
+function App() {
+  return (
+    <div>
+      <BrowseRouter></BrowseRouter>
+    </div>
+  );
+}
+
+export default App;
+
+
